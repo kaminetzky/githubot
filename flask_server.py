2104d02e8dd341e08f2b021a29ae598733e29ea1
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-03 23:12:59 -0400

    Add message checking

--- flask_server.py
+++ flask_server.py
@@ -31,14 +31,15 @@ class MyApp(flask.Flask):
         def telegram_post():
             update = flask.request.get_json()
             if 'message' in update:
-                chat_id = update['message']['chat']['id']
-                if update['message']['chat']['type'] == 'group':
-                    reply_text = self.telegram_request_processor\
-                        .process_request(update)
-                else:
-                    reply_text = ('Solo estoy hecho para funcionar con un '
-                                  'grupo. ¡Lo siento!')
-                self.telegram.send_message(chat_id, reply_text)
+                if 'text' in update['message']:
+                    chat_id = update['message']['chat']['id']
+                    if update['message']['chat']['type'] == 'group':
+                        reply_text = self.telegram_request_processor\
+                            .process_request(update)
+                    else:
+                        reply_text = ('Solo estoy hecho para funcionar con un '
+                                      'grupo. ¡Lo siento!')
+                    self.telegram.send_message(chat_id, reply_text)
             return ''
 
         @self.route('/post/github', methods=['POST'])
