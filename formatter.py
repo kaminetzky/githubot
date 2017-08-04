a2e883dadd492224b7113cb10cc36150aebf5813
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-04 11:16:12 -0400

    Modify get command formatting

--- formatter.py
+++ formatter.py
@@ -1,9 +1,9 @@
 class Formatter:
     @staticmethod
     def format_issue(author, number, title, text, url):
-        string = ('<i>[{}]</i>\n'
+        string = ('<b>[{}]</b>\n'
                   '<b>[#{} - {}]</b>\n\n'
                   '{}\n\n'
-                  '<a href="{}">[Link]</a>'.format(author, number, title, text,
+                  '<a href="{}">Link</a>'.format(author, number, title, text,
                                                  url))
         return string
