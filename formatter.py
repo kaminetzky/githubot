612cb8059f41c0c1b32f8137b5c378cfd28f1e0f
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-22 18:35:58 -0300

    Crop body if it's too long

--- formatter.py
+++ formatter.py
@@ -1,6 +1,9 @@
 class Formatter:
     @staticmethod
     def format_issue(author, number, title, text, url):
+        max_length = 4000
+        if len(title + text) > max_length:
+            text = text[:max_length - title] + '...'
         string = ('<b>Issue #{}</b>\n'
                   '<b>Título:</b> {}\n'
                   '<b>Autor:</b> {}\n\n'
