173aa5e2e338af53076d93013e2a3aa2cf1be643
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2018-08-19 00:51:21 -0300

    Add config var checker

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 telegram_token = os.environ['telegram_token']
-authorized_chats = list(map(int, os.environ['authorized_chats'].split()))
-broadcast_chats = list(map(int, os.environ['broadcast_chats'].split()))
-main_chat = int(os.environ['main_chat'])
-tareos_chat = int(os.environ['tareos_chat'])
+authorized_chats = (list(map(int, os.environ['authorized_chats'].split()))
+                    if os.environ['authorized_chats'] else [])
+broadcast_chats = (list(map(int, os.environ['broadcast_chats'].split()))
+                   if os.environ['broadcast_chats'] else [])
+main_chat = int(os.environ['main_chat']) if os.environ['main_chat'] else 0
+tareos_chat = int(os.environ['tareos_chat']) if os.environ['tareos_chat'] else 0
 channel_chat = os.environ['channel_chat']
 
 github_token = os.environ['github_token']
